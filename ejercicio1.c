1cf56d1b5f463ce30518cd0d8b3fa4ef33255feb
Andre Revilla <andre.revilla@ucsp.edu.pe>
2026-08-28 18:32:39 +0000

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -10,23 +10,20 @@ static sigjmp_buf salto_sigusr2;
 
 void handler_sigint(int signal)
 {
-    printf("\nSe recibió la señal %d SIGINT\n", signal);
-
-    // Salto de ejecución al punto establecido con sigsetjmp()
+    printf("\n[ SIGINT recibida ]\n\n");
+    printf(">>> Ejecutando salto no local #1 \n");
     siglongjmp(salto_sigint, 1);
 }
 void handler_sigusr1(int signal)
 {
-    printf("\nSe recibió la señal %d SIGUSR1\n", signal);
-
-    // Salto de ejecución al punto establecido con sigsetjmp()
+    printf("\n[ SIGUSR1 recibida ]\n\n");
+    printf(">>> Ejecutando salto no local #2 \n");
     siglongjmp(salto_sigusr1, 1);
 }
 void handler_sigusr2(int signal)
 {
-    printf("\nSe recibió la señal %d SIGUSR2\n", signal);
-
-    // Salto de ejecución al punto establecido con sigsetjmp()
+    printf("\n[ SIGUSR2 recibida ]\n\n");
+    printf(">>> Ejecutando salto no local #3 \n");
     siglongjmp(salto_sigusr2, 1);
 }
 
@@ -42,26 +39,24 @@ int main(void)
 
     while(1){
 
-        if (sigsetjmp(salto_sigint, 1) != 0) {
-            printf(">>> Ejecutando salto no local #1 \n");
-            printf(">>> Recuperacion en punto SIGINT \n\n");
-            printf("Continuando desde el punto de recuperación...\n");
-        }
-    
-        if (sigsetjmp(salto_sigusr1, 1) != 0) {
-            printf(">>> Ejecutando salto no local #2 \n");
-            printf(">>> Recuperacion en punto SIGUSR1 \n\n");
-            printf("Continuando desde el punto de recuperación...\n");
-        }
-    
-        if (sigsetjmp(salto_sigusr2, 1) != 0) {
-            printf(">>> Ejecutando salto no local #3 \n");
-            printf(">>> Recuperacion en punto SIGUSR2 \n\n");
-            printf("Continuando desde el punto de recuperación...\n");
-        }
-    
-        printf("Esperando señales...\n");
-        sleep(2);
+    if (sigsetjmp(salto_sigint, 1) != 0) {
+        printf(">>> Recuperacion en punto SIGINT \n\n");
+        printf("Programa continua ejecutandose desde el punto de recuperación...\n\n");
+    }
+
+    if (sigsetjmp(salto_sigusr1, 1) != 0) {
+        printf(">>> Recuperacion en punto SIGUSR1 \n\n");
+        printf("Programa continua ejecutandose desde el punto de recuperación...\n\n");
+    }
+
+    if (sigsetjmp(salto_sigusr2, 1) != 0) {
+        printf(">>> Recuperacion en punto SIGUSR2 \n\n");
+        printf("Programa continua ejecutandose desde el punto de recuperación...\n\n");
+    }
+
+    printf("Esperando señales...\n");
+    sleep(2);
+
     }
 
     printf("Fin del programa.\n");
