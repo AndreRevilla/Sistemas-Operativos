72e61936eb0129c6a1f18d2c7e10fea025da0ce3
Andre Revilla <andre.revilla@ucsp.edu.pe>
2026-08-28 18:14:19 +0000

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <setjmp.h>
 #include <unistd.h>
 
-static sigjmp_buf punto_salto;
+static sigjmp_buf salto_sigint;
 static sigjmp_buf salto_sigusr1;
 static sigjmp_buf salto_sigusr2;
 
@@ -12,7 +13,7 @@ void handler_sigint(int signal)
     printf("\nSe recibió la señal %d SIGINT\n", signal);
 
     // Salto de ejecución al punto establecido con sigsetjmp()
-    siglongjmp(punto_salto, 1);
+    siglongjmp(salto_sigint, 1);
 }
 void handler_sigusr1(int signal)
 {
@@ -36,17 +37,31 @@ int main(void)
     signal(SIGUSR1, handler_sigusr1);
     signal(SIGUSR2, handler_sigusr2);
 
-    if (sigsetjmp(punto_salto, 1) == 0) {
-        printf("Programa iniciado.\n");
+    printf("PID del proceso: %d \n", getpid());
+    printf("Programa iniciado.\n");
 
-        while (1) {
-            printf("Ejecutando código normal...\n");
-            sleep(2);
+    while(1){
+
+        if (sigsetjmp(salto_sigint, 1) != 0) {
+            printf(">>> Ejecutando salto no local #1 \n");
+            printf(">>> Recuperacion en punto SIGINT \n\n");
+            printf("Continuando desde el punto de recuperación...\n");
         }
-    }   else {
-        // La ejecución llega aquí después de recibir SIGINT
-        printf("¡Se realizó el salto de código!\n");
-        printf("Continuando desde el punto de recuperación...\n");
+    
+        if (sigsetjmp(salto_sigusr1, 1) != 0) {
+            printf(">>> Ejecutando salto no local #2 \n");
+            printf(">>> Recuperacion en punto SIGUSR1 \n\n");
+            printf("Continuando desde el punto de recuperación...\n");
+        }
+    
+        if (sigsetjmp(salto_sigusr2, 1) != 0) {
+            printf(">>> Ejecutando salto no local #3 \n");
+            printf(">>> Recuperacion en punto SIGUSR2 \n\n");
+            printf("Continuando desde el punto de recuperación...\n");
+        }
+    
+        printf("Esperando señales...\n");
+        sleep(2);
     }
 
     printf("Fin del programa.\n");
